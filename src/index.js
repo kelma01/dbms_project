7a73a95c8bb8667d9821979d47e3b3c9524ecf45
Kerem Elma <123359061+kelma01@users.noreply.github.com>
2024-07-10 00:07:04 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 const PORT = 3001;
+const movieRoute = require('./routes/movies')
+const cinemaRoute = require('./routes/cinemas')
 
 //post req için kullanılan bir middleware
 app.use(express.json());
@@ -11,52 +13,25 @@ app.use((req, res, next) => {
     next();
 });
 
+//gorcery route kullanılırken artik basina api path'i alacak get ve post requestlerde mesela postmandan atılırken
+app.use('/api/movies', movieRoute);
+app.use('/api/cinemas', cinemaRoute);
+
 //localhost initialization
 app.listen(PORT, () => console.log(`Running Server on Port ${PORT}`));
 
 //db initialization
+//eger authentication saglanamazsaMySQL workbench'e yandaki command'i gir -> ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_şifreniz';
 const connection = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
         password: 'kerem',
         database: 'dbms_project',
-        authPlugins: {
-            mysql_native_password: mysql.authPlugins.mysql_native_password
-        }
     }
 );
 
 connection.connect();
 
 
-//GET ve POST request ornekleri
-const groceryList = [
-    {
-        item: 'water',
-        quantity: 4
-    },
-    {
-        item: 'food',
-        quantity: 5
-    }
-]
-//GET req
-app.get('/groceries', 
-        (req, res, next) => {   //buradaki next olayı yollanılan get req'in bekletilmesini sağlar. karşılığında bir aksiyon bekler
-            console.log("Before handling request.")
-            next();//bu bekleme olayını iptal ettiriri normal hale sokar
-        }, 
-        (req, res, next) => {
-        res.send(groceryList);
-        next();
-        }
-        )
-
-//buradaki request postman'dan gelen post request body'sidir.
-app.post('/groceries', (request, response) => {
-    console.log(request.body);
-    groceryList.push(request.body);
-    response.send(201); //http status
-    //postman ile POST requestler yapılabilr test edilebilir.
-});
+connection.end();
