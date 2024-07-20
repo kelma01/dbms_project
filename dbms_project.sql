13ed81ac432373e2229e5abe125720773b0c2e78
Kerem Elma <123359061+kelma01@users.noreply.github.com>
2024-07-20 23:06:38 +0300

    userid datatype değiştiği için constraintler düzeltildi

--- dbms_project.sql
+++ dbms_project.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `dbms_project` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `dbms_project`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: dbms_project
@@ -28,7 +26,7 @@ CREATE TABLE `actor_actress` (
   `name` varchar(45) NOT NULL,
   `surname` varchar(45) NOT NULL,
   `movie_id` int NOT NULL,
-  PRIMARY KEY (`movie_id`,`surname`,`name`),
+  KEY `act_movie_id` (`movie_id`),
   CONSTRAINT `act_movie_id` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -39,7 +37,6 @@ CREATE TABLE `actor_actress` (
 
 LOCK TABLES `actor_actress` WRITE;
 /*!40000 ALTER TABLE `actor_actress` DISABLE KEYS */;
-INSERT INTO `actor_actress` VALUES ('Lucy','Hale',1),('Michael','Pena',1),('Eddie','Murphy',2),('Taylour','Paige',2);
 /*!40000 ALTER TABLE `actor_actress` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -67,7 +64,6 @@ CREATE TABLE `cinema` (
 
 LOCK TABLES `cinema` WRITE;
 /*!40000 ALTER TABLE `cinema` DISABLE KEYS */;
-INSERT INTO `cinema` VALUES (1,'Cinemaximum','Panora','Ankara','Oran, Kudüs Cd. No:3, 06450 Çankaya/Ankara','3'),(2,'Paribu','Armada','Ankara','Beştepe, Dumlupınar Blv. No:6, 06560 Yenimahalle/Ankara','3');
 /*!40000 ALTER TABLE `cinema` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,9 +79,9 @@ CREATE TABLE `credit card` (
   `ccv` int NOT NULL,
   `expiration_year` varchar(45) DEFAULT NULL,
   `expiration_month` varchar(45) DEFAULT NULL,
-  `wallet_id` int NOT NULL,
-  PRIMARY KEY (`wallet_id`),
-  CONSTRAINT `wallet_id` FOREIGN KEY (`wallet_id`) REFERENCES `wallet` (`wallet_id`)
+  `wallet_id` varchar(36) NOT NULL,
+  KEY `credit_cards_wallet_id` (`wallet_id`),
+  CONSTRAINT `credit_cards_wallet_id` FOREIGN KEY (`wallet_id`) REFERENCES `wallet` (`wallet_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -95,7 +91,6 @@ CREATE TABLE `credit card` (
 
 LOCK TABLES `credit card` WRITE;
 /*!40000 ALTER TABLE `credit card` DISABLE KEYS */;
-INSERT INTO `credit card` VALUES (63223732487,313,'2027','4',1);
 /*!40000 ALTER TABLE `credit card` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -113,10 +108,10 @@ CREATE TABLE `filter` (
   `f_rating` varchar(45) DEFAULT NULL,
   `f_is_released` tinyint DEFAULT NULL,
   `f_genre` varchar(45) DEFAULT NULL,
-  `user_id` int NOT NULL,
   `cinema_id` int NOT NULL,
-  PRIMARY KEY (`user_id`,`cinema_id`),
-  KEY `filter_cinema_id_idx` (`cinema_id`),
+  `user_id` varchar(36) NOT NULL,
+  KEY `filter_cinema_id` (`cinema_id`),
+  KEY `filter_user_id` (`user_id`),
   CONSTRAINT `filter_cinema_id` FOREIGN KEY (`cinema_id`) REFERENCES `cinema` (`cinema_id`),
   CONSTRAINT `filter_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -128,7 +123,6 @@ CREATE TABLE `filter` (
 
 LOCK TABLES `filter` WRITE;
 /*!40000 ALTER TABLE `filter` DISABLE KEYS */;
-INSERT INTO `filter` VALUES ('Ankara','Cinemaximum','Tuesday','0-1',1,'horror',1,1);
 /*!40000 ALTER TABLE `filter` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -160,7 +154,6 @@ CREATE TABLE `movie` (
 
 LOCK TABLES `movie` WRITE;
 /*!40000 ALTER TABLE `movie` DISABLE KEYS */;
-INSERT INTO `movie` VALUES (1,0.40,'2h','horror','Hayal Adasi',5.60,'Blumhouse\'un Fantezi Adası olarak da bilinen Fantasy Island, Jeff Wadlow\'un yönettiği ve ortak yazdığı 2020 Amerikan doğaüstü korku filmidir.','10.50, 23.30','all week',1),(2,0.90,'2h','comedy','Sosyete Polisi 4',4.00,'Sosyete Polisi: Axel F, Mark Molloy tarafından yönetilen, Will Beall, Tom Gormican ve Kevin Etten tarafından Beall\'ın bir hikayesinden yazılan yaklaşmakta olan bir Amerikan aksiyon komedi filmi','12.30, 21.20','all week',0);
 /*!40000 ALTER TABLE `movie` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -173,12 +166,12 @@ DROP TABLE IF EXISTS `rates`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `rates` (
   `r_comment` varchar(45) DEFAULT NULL,
-  `r_star` int NOT NULL,
-  `r_date` date NOT NULL,
-  `user_id` int NOT NULL,
+  `r_star` int DEFAULT NULL,
+  `r_date` date DEFAULT NULL,
   `movie_id` int NOT NULL,
-  PRIMARY KEY (`movie_id`,`user_id`),
-  KEY `rates_user_id_idx` (`user_id`),
+  `user_id` varchar(36) NOT NULL,
+  KEY `rates_movie_id` (`movie_id`),
+  KEY `rates_user_id` (`user_id`),
   CONSTRAINT `rates_movie_id` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`),
   CONSTRAINT `rates_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -190,7 +183,6 @@ CREATE TABLE `rates` (
 
 LOCK TABLES `rates` WRITE;
 /*!40000 ALTER TABLE `rates` DISABLE KEYS */;
-INSERT INTO `rates` VALUES (NULL,3,'2023-03-20',1,1);
 /*!40000 ALTER TABLE `rates` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -221,7 +213,6 @@ CREATE TABLE `seat` (
 
 LOCK TABLES `seat` WRITE;
 /*!40000 ALTER TABLE `seat` DISABLE KEYS */;
-INSERT INTO `seat` VALUES (15,0,2,1),(25,1,2,1);
 /*!40000 ALTER TABLE `seat` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -250,7 +241,6 @@ CREATE TABLE `theatre` (
 
 LOCK TABLES `theatre` WRITE;
 /*!40000 ALTER TABLE `theatre` DISABLE KEYS */;
-INSERT INTO `theatre` VALUES (2,60,'',1),(3,80,'',2);
 /*!40000 ALTER TABLE `theatre` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -269,13 +259,13 @@ CREATE TABLE `ticket` (
   `theater_id` int NOT NULL,
   `seat_id` int NOT NULL,
   `movie_id` int NOT NULL,
-  `user_id` int NOT NULL,
+  `user_id` varchar(36) NOT NULL,
   PRIMARY KEY (`ticket_id`),
   UNIQUE KEY `theater_id_UNIQUE` (`theater_id`),
   UNIQUE KEY `seat_id_UNIQUE` (`seat_id`),
   UNIQUE KEY `movie_id_UNIQUE` (`movie_id`),
-  UNIQUE KEY `user_id_UNIQUE` (`user_id`),
   KEY `ticket_cinema_id_idx` (`cinema_id`),
+  KEY `ticket_user_id` (`user_id`),
   CONSTRAINT `ticket_cinema_id` FOREIGN KEY (`cinema_id`) REFERENCES `cinema` (`cinema_id`),
   CONSTRAINT `ticket_movie_id` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`),
   CONSTRAINT `ticket_seat_id` FOREIGN KEY (`seat_id`) REFERENCES `seat` (`seat_id`),
@@ -290,7 +280,6 @@ CREATE TABLE `ticket` (
 
 LOCK TABLES `ticket` WRITE;
 /*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
-INSERT INTO `ticket` VALUES (1,'Tuesday','23.30',1,2,15,1,1);
 /*!40000 ALTER TABLE `ticket` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -302,30 +291,18 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `user_id` int NOT NULL,
+  `user_id` varchar(36) NOT NULL,
   `name` varchar(45) NOT NULL,
   `surname` varchar(45) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `password` varchar(45) NOT NULL,
   `movies_watched` varchar(45) DEFAULT NULL,
   `birth_date` date DEFAULT NULL,
   `tickets` varchar(45) DEFAULT NULL,
-  `wallet_id` int NOT NULL,
-  `email` varchar(100) NOT NULL,
-  PRIMARY KEY (`user_id`),
-  KEY `user_wallet_id` (`wallet_id`),
-  CONSTRAINT `user_wallet_id` FOREIGN KEY (`wallet_id`) REFERENCES `wallet` (`wallet_id`)
+  `email` varchar(45) NOT NULL,
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'neva','varol','734','1','2024-04-20','1',1),(2,'kerem','elma','823','1',NULL,'0',2),(3,'halil melih','akca','325','1',NULL,'0',3),(4,'mehmet','eski','466','1',NULL,'0',4);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `wallet`
@@ -338,9 +315,12 @@ CREATE TABLE `wallet` (
   `tickets` varchar(45) DEFAULT NULL,
   `balance` varchar(45) NOT NULL,
   `coupons` varchar(45) DEFAULT NULL,
-  `wallet_id` int NOT NULL,
+  `wallet_id` varchar(36) NOT NULL,
   `credit_cards` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`wallet_id`)
+  `user_id` varchar(36) NOT NULL,
+  PRIMARY KEY (`wallet_id`),
+  KEY `wallet_user_id` (`user_id`),
+  CONSTRAINT `wallet_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -350,7 +330,6 @@ CREATE TABLE `wallet` (
 
 LOCK TABLES `wallet` WRITE;
 /*!40000 ALTER TABLE `wallet` DISABLE KEYS */;
-INSERT INTO `wallet` VALUES ('1','0','A',1,NULL),('1','0','B',2,NULL),('1','0','C',3,NULL),('1','200','D',4,NULL),(NULL,'0',NULL,505,NULL);
 /*!40000 ALTER TABLE `wallet` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -363,4 +342,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-07-20 20:21:19
+-- Dump completed on 2024-07-20 23:04:52
