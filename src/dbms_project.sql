3f4e7ee3ee723dc963897c641681d91696d50af2
Kerem Elma <123359061+kelma01@users.noreply.github.com>
2024-07-21 18:31:35 +0300

    bug fix, constraintler seat, theater, cinema için düzenlendi

--- src/dbms_project.sql
+++ src/dbms_project.sql
@@ -13,11 +13,8 @@
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */; 
 
---
--- Table structure for table `actor_actress`
---
 
 DROP TABLE IF EXISTS `actor_actress`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
